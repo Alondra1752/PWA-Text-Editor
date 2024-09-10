9120873dc6174473d859ac5941fa4a8095a34325
Alondra1752 <alondrasant7739@gmail.com>
2024-09-10 16:50:18 -0500

    added delete on the client side

---
+++ client/src/js/delete.js
@@ -0,0 +1,17 @@
+import { deleteDb } from "./database";
+
+export const setupDeleteButton = (editor) => {
+  const butDelete = document.getElementById("buttonDelete");
+
+  // Click event handler on the `butDelete` element
+  butDelete.addEventListener("click", async () => {
+    console.log("delete button clicked!");
+    localStorage.removeItem("content");
+    await deleteDb();
+    editor.setValue(`
+    
+Your TextOG notes have been cleared! 
+     `);
+  });
+};
+
